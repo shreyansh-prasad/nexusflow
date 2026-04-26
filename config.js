9d66799ff7309c7fc83b1a5d219e8ea1b0abe5e5
shreyansh-prasad <shreyanshtrillion@gmail.com>
2026-04-26 12:54:36 +0530

    Update NEXUSFLOW_BACKEND URL to production

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-window.NEXUSFLOW_BACKEND = 'http://localhost:8001';
+window.NEXUSFLOW_BACKEND = 'https://nexusflow-xj19.onrender.com';
 window.NEXUSFLOW_SUPABASE_URL = window.NEXUSFLOW_SUPABASE_URL || 'https://zodkyaasljddsraqvqkz.supabase.co';
 window.NEXUSFLOW_SUPABASE_KEY = window.NEXUSFLOW_SUPABASE_KEY || 'sb_publishable_9JYWOShowXPJ818ZGELB4A_JNVm5vzQ';
